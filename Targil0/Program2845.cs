a717bb1ee875e90543e860a0a043d0e78954d837
Ariel M <amoreshe@g.jct.ac.il>
2021-10-21 10:27:59 +0300

    chaneges

---
+++ Targil0/Program2845.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome2845();
+            welcome5562();
+            Console.ReadKey();
+        }
+        static partial void welcome5562();
+        private static void welcome2845()
+        {
+            Console.WriteLine("enter your name:");
+            string username = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", username);
+        }
+    }
+}
