9c79f331e75acdb3d3729e8372a238c7030f0adb
YairBybabayov <bybabayo@g.jct.ac.il>
2021-10-22 12:29:19 +0300

    im  also here

---
+++ Targil0/program5562.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void welcome5562() { Console.WriteLine("Im also here"); }
+
+    }
+}
