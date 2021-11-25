ce3fa9df753a76e32df6cad78eef1c08ecdac295
Ariel M <amoreshe@g.jct.ac.il>
2021-11-25 17:25:33 +0200

    bla bla

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,11 +2,36 @@
 
 namespace ConsoleUI
 {
-    class Program
+    class Program   
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hey");
+            string Option;
+            Console.WriteLine("Hey you got to ariel&babauv drone's");
+            Console.WriteLine("Choose what you want to do");
+            Console.WriteLine("Type what you want to do from the list below");
+            Console.WriteLine("Addition, Updating, Display, List view or Exit");
+            Option = Console.ReadLine();
+            switch (Option)
+            {
+                case "Addition":
+                    FunAddition();
+                    break;
+                case "Updating":
+                    FunUpdating();
+                    break;
+                case "Display":
+                    FunDisplay();
+                    break;
+                case "List view":
+                    FunListview();
+                    break;
+                case "Exit":
+                    Console.WriteLine("Thank you have a good day");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
