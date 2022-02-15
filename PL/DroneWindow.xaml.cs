b1fb084ad22ab246ec9011172367229486a660b9
YairBybabayov <bybabayo@g.jct.ac.il>
2022-02-15 23:06:01 +0200

    WEIRD

--- PL/DroneWindow.xaml.cs
+++ PL/DroneWindow.xaml.cs
@@ -23,58 +23,58 @@ namespace PL
     /// </summary>
     public partial class DroneWindow : Window
     {
-        //#region disable Close Button
-        //protected override void OnSourceInitialized(EventArgs e)
-        //{
-        //    base.OnSourceInitialized(e);
+        #region disable Close Button
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
 
-        //    HwndSource hwndSource = PresentationSource.FromVisual(this) as HwndSource;
+            HwndSource hwndSource = PresentationSource.FromVisual(this) as HwndSource;
 
-        //    if (hwndSource != null)
-        //    {
-        //        hwndSource.AddHook(HwndSourceHook);
-        //    }
+            if (hwndSource != null)
+            {
+                hwndSource.AddHook(HwndSourceHook);
+            }
 
-        //}
+        }
 
-        //private bool allowClosing = false;
+        private bool allowClosing = false;
 
-        //[DllImport("user32.dll")]
-        //private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
-        //[DllImport("user32.dll")]
-        //private static extern bool EnableMenuItem(IntPtr hMenu, uint uIDEnableItem, uint uEnable);
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+        [DllImport("user32.dll")]
+        private static extern bool EnableMenuItem(IntPtr hMenu, uint uIDEnableItem, uint uEnable);
 
-        //private const uint MF_BYCOMMAND = 0x00000000;
-        //private const uint MF_GRAYED = 0x00000001;
+        private const uint MF_BYCOMMAND = 0x00000000;
+        private const uint MF_GRAYED = 0x00000001;
 
-        //private const uint SC_CLOSE = 0xF060;
+        private const uint SC_CLOSE = 0xF060;
 
-        //private const int WM_SHOWWINDOW = 0x00000018;
-        //private const int WM_CLOSE = 0x10;
+        private const int WM_SHOWWINDOW = 0x00000018;
+        private const int WM_CLOSE = 0x10;
 
-        //private IntPtr HwndSourceHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-        //{
-        //    switch (msg)
-        //    {
-        //        case WM_SHOWWINDOW:
-        //            {
-        //                IntPtr hMenu = GetSystemMenu(hwnd, false);
-        //                if (hMenu != IntPtr.Zero)
-        //                {
-        //                    EnableMenuItem(hMenu, SC_CLOSE, MF_BYCOMMAND | MF_GRAYED);
-        //                }
-        //            }
-        //            break;
-        //        case WM_CLOSE:
-        //            if (!allowClosing)
-        //            {
-        //                handled = true;
-        //            }
-        //            break;
-        //    }
-        //    return IntPtr.Zero;
-        //}
-        //#endregion
+        private IntPtr HwndSourceHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case WM_SHOWWINDOW:
+                    {
+                        IntPtr hMenu = GetSystemMenu(hwnd, false);
+                        if (hMenu != IntPtr.Zero)
+                        {
+                            EnableMenuItem(hMenu, SC_CLOSE, MF_BYCOMMAND | MF_GRAYED);
+                        }
+                    }
+                    break;
+                case WM_CLOSE:
+                    if (!allowClosing)
+                    {
+                        handled = true;
+                    }
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+        #endregion
 
         private readonly IBL.IBL bl = BL.BL.GetInstance();
 
