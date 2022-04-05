994105db7c59c30733582a8eaee32fc049c8d16d
YairBybabayov <bybabayo@g.jct.ac.il>
2022-04-05 12:49:47 +0300

    עכעכע

---
+++ DAL/DO/XMLFileLoadCreateException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DO
+{
+    [Serializable]
+    internal class XMLFileLoadCreateException : Exception
+    {
+        private string filePath;
+        private string v;
+        private Exception ex;
+
+        public XMLFileLoadCreateException()
+        {
+        }
+
+        public XMLFileLoadCreateException(string message) : base(message)
+        {
+        }
+
+        public XMLFileLoadCreateException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public XMLFileLoadCreateException(string filePath, string v, Exception ex)
+        {
+            this.filePath = filePath;
+            this.v = v;
+            this.ex = ex;
+        }
+
+        protected XMLFileLoadCreateException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
