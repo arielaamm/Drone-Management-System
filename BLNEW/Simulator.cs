ec559afe5c1863319ebaf545a5f75a2b97b297ed
YairBybabayov <bybabayo@g.jct.ac.il>
2022-04-17 16:41:15 +0300

    simu

--- BLNEW/Simulator.cs
+++ BLNEW/Simulator.cs
@@ -8,6 +8,83 @@ namespace BL
         private readonly int DELAY = 1000; // whaiting time 1 sec(1000 mlsc)
         public Simulator(BL bl, int droneId, Action display, bool checker)//constractor
         {
+<<<<<<< HEAD
+            while (!checker)
+            {
+                Drone d = new Drone();
+                d = bl.FindDrone(droneId);
+                switch (d.Status)
+                {
+                    case Status.CREAT:
+                        try
+                        {
+                            bl.AttacheDrone(droneId);
+                        }
+                        catch (DontHaveEnoughPowerException)
+                        {
+                            try
+                            {
+                                bl.DroneToCharge(droneId);
+                            }
+                            catch
+                            {
+                                //TODO
+                                //  throw new BO.CantBeNegative(droneId);
+                            }
+                        }
+                        catch (ThereIsNoParcelToAttachException)
+                        {
+                            Thread.Sleep(DELAY);
+                        }
+                        break;
+                    case Status.BELONG:
+                        if (bl.Findparcel((int)(d.Parcel.ID)).PickedUp == null)
+                        {
+                            double time = Distance(d.Position, d.Parcel.LocationOfSender) / speed;//enough battery??
+                            if (time < 1)
+                            {
+                                bl.PickUpParcel((int)(d.Parcel.ID));
+                                //i = 0;
+                                Thread.Sleep((int)(Distance(d.Position, d.Parcel.LocationOfSender) / speed));
+                            }
+                            else
+                            {
+                                Thread.Sleep(DELAY);
+                                //bl.AddBattery(droneId, -bl.GetElectricityPerKM((DELAY / 1000) * speed, d.Parcel.weight));
+                                //TODO bonus location
+                            }
+                        }
+                        else
+                        {
+                            double time = Distance(d.Position, d.Parcel.LocationOftarget) / speed;// - i;//enough battery??
+                            if (time < 1)
+                            {
+                                bl.Parceldelivery((int)d.Parcel.ID);
+                                //i = 0;
+                                Thread.Sleep((int)(Distance(d.Position, d.Parcel.LocationOfSender) / speed));
+                                d.Parcel.distance = 0;
+                            }
+                            else
+                            {
+                                Thread.Sleep(DELAY);
+                                //d.Parcel.distance -= (DELAY / 1000) * speed;
+                            }
+
+                        }
+                        break;
+                    case Status.MAINTENANCE:
+                        Thread.Sleep(DELAY);
+                        if (d.Battery == 100)
+                        {
+                            bl.DroneOutCharge(droneId, 0.1);
+                            //i = 0;
+                            break;
+                        }
+                        //bl.AddBattery(droneId, (DELAY / 1000) * bl.ChargePerSecond);
+                        break;
+                }
+            }
+=======
             //while (!checker)
             //{
             //    Drone d = new Drone();
@@ -85,6 +162,7 @@ namespace BL
             //            break;
             //    }
             //}
+>>>>>>> 24814bf98c233ab363c1dc0a584770dcbd4fdc06
         }
 
     }
